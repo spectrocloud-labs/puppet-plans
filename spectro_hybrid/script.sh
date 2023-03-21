9e13a2f3b04f5f905288cf09ca280bce483651c9
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-21 16:13:11 +0100

    Create script.sh
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

---
+++ spectro_hybrid/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget https://apt.puppet.com/puppet-tools-release-focal.deb -O /tmp/puppet-tools-release-focal.deb
+dpkg -i /tmp/puppet-tools-release-focal.deb
+apt-get update
+apt-get install -y puppet-bolt
+apt-get install -y qemu-guest-agent
+mkdir /root/spectro_install
+cd /root/spectro_install
+bolt project init spectro_mysql --modules puppetlabs-mysql
+mkdir /root/spectro_install/plans
+curl "https://raw.githubusercontent.com/spectrocloud-labs/puppet-plans/main/spectro_mysql/init.pp" -o /root/spectro_install/plans/init.pp
+cd /root/spectro_install
+bolt plan run spectro_mysql
+wget https://raw.githubusercontent.com/dockersamples/wordsmith/main/db/words.sql -O /tmp/words.sql
+systemctl start qemu-guest-agent
