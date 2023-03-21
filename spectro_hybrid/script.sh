14b0a12e75b7bca904f9a39bec4ec124131b6a58
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-21 17:12:13 +0100

    Update script.sh
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/script.sh
+++ spectro_hybrid/script.sh
@@ -6,7 +6,7 @@ apt-get install -y puppet-bolt
 apt-get install -y qemu-guest-agent
 mkdir /root/spectro_install
 cd /root/spectro_install
-bolt project init spectro_mysql --modules puppetlabs-mysql
+bolt project init spectro_postgres --modules puppetlabs-postgresql
 mkdir /root/spectro_install/plans
 curl "https://raw.githubusercontent.com/spectrocloud-labs/puppet-plans/main/spectro_hybrid/init.pp" -o /root/spectro_install/plans/init.pp
 cd /root/spectro_install
