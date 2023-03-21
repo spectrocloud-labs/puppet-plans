d01da0a92cc219938fc3bad0aa13d942dbf6dd68
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-21 17:51:56 +0100

    Update script.sh
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/script.sh
+++ spectro_hybrid/script.sh
@@ -12,5 +12,5 @@ curl "https://raw.githubusercontent.com/spectrocloud-labs/puppet-plans/main/spec
 cd /root/spectro_install
 bolt plan run spectro_postgres
 wget https://raw.githubusercontent.com/dockersamples/wordsmith/main/db/words.sql -O /tmp/words.sql
-psql mydatabase < /tmp/words.sql
+sudo -u postgres psql db < /tmp/words.sql
 systemctl start qemu-guest-agent
