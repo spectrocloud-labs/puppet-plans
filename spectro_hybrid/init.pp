4a2bf90c71db7ecaacbefef0517197ea1a3588da
Codrin Iftimie <iceboxic@gmail.com>
2023-03-23 23:16:29 +0200

    Cleanup

--- spectro_hybrid/init.pp
+++ spectro_hybrid/init.pp
@@ -4,17 +4,11 @@ plan spectro_postgres(){
 
     class { 'postgresql::server':
       listen_addresses => '0.0.0.0',
-      ensure => '10.0',
-      postgres_password => '',
     }
 
     postgresql::server::db { 'postgres':
       user     => 'postgres',
       password => postgresql::postgresql_password('postgres', ''),
-    }
-
-    package { 'postgresql-client':
-      ensure => 'installed',
     }
   }
 
--- spectro_hybrid/script.sh
+++ spectro_hybrid/script.sh
@@ -2,8 +2,7 @@
 wget https://apt.puppet.com/puppet-tools-release-focal.deb -O /tmp/puppet-tools-release-focal.deb
 dpkg -i /tmp/puppet-tools-release-focal.deb
 apt-get update
-apt-get install -y puppet-bolt
-apt-get install -y qemu-guest-agent postgresql-client
+apt-get install -y puppet-bolt qemu-guest-agent postgresql-client-common
 mkdir /root/spectro_install
 cd /root/spectro_install
 bolt project init spectro_postgres --modules puppetlabs-postgresql
@@ -12,5 +11,5 @@ curl "https://raw.githubusercontent.com/spectrocloud-labs/puppet-plans/main/spec
 cd /root/spectro_install
 bolt plan run spectro_postgres
 wget https://raw.githubusercontent.com/dockersamples/wordsmith/main/db/words.sql -O /tmp/words.sql
-sudo -u postgres psql postgres < /tmp/words.sql
+sudo psql postgres < /tmp/words.sql
 systemctl start qemu-guest-agent
