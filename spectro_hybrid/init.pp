dfa8843bc5318c487b406ee181068ce91ddc8bca
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-24 16:37:58 +0100

    Update init.pp
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/init.pp
+++ spectro_hybrid/init.pp
@@ -6,7 +6,16 @@ plan spectro_postgres(){
       listen_addresses           => '0.0.0.0',
       ip_mask_deny_postgres_user => '0.0.0.0/32', # don't deny from anywhere
       ip_mask_allow_all_users    => '0.0.0.0/0',  # allow all users from anywhere
-      password_encryption        => 'md5',        # need to switch this away from scram-sha-256
+      #password_encryption        => 'md5',        # need to switch this away from scram-sha-256
+    }
+
+    postgresql::server::pg_hba_rule { 'allow wordsmith api pods to access db database':
+      description => 'Open up PostgreSQL for access from anywhere',
+      type        => 'host',
+      database    => 'db',
+      user        => 'postgres',
+      address     => '0.0.0.0/0',
+      auth_method => 'trust',
     }
 
     postgresql::server::db { 'db':
