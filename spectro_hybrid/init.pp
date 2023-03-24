29d83dac71f6a6e192512faf3182b4fdf058c1b5
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-24 16:54:45 +0100

    Update init.pp
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/init.pp
+++ spectro_hybrid/init.pp
@@ -5,7 +5,6 @@ plan spectro_postgres(){
     class { 'postgresql::server':
       listen_addresses           => '0.0.0.0',
       ip_mask_deny_postgres_user => '0.0.0.0/32', # don't deny from anywhere
-      ip_mask_allow_all_users    => '0.0.0.0/0',  # allow all users from anywhere
     }
 
     postgresql::server::pg_hba_rule { 'allow wordsmith api pods to access db database':
