2eb54e86f053bc395a837fd1807abd1d76748af2
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-24 16:17:48 +0100

    Set pwd encryption to md5
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/init.pp
+++ spectro_hybrid/init.pp
@@ -6,6 +6,7 @@ plan spectro_postgres(){
       listen_addresses           => '0.0.0.0',
       ip_mask_deny_postgres_user => '0.0.0.0/32', # don't deny from anywhere
       ip_mask_allow_all_users    => '0.0.0.0/0',  # allow all users from anywhere
+      password_encryption        => 'md5',        # need to switch this away from scram-sha-256
     }
 
     postgresql::server::db { 'db':
