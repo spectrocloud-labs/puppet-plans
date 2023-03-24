2092f1dff2c3129be9974da1337e20fb4a4ea592
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-24 14:55:48 +0100

    Set dummy db user
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/init.pp
+++ spectro_hybrid/init.pp
@@ -3,12 +3,14 @@ plan spectro_postgres(){
   apply(localhost) {
 
     class { 'postgresql::server':
-      listen_addresses => '0.0.0.0',
+      listen_addresses           => '0.0.0.0',
+      ip_mask_deny_postgres_user => '0.0.0.0/32', # don't deny from anywhere
+      ip_mask_allow_all_users    => '0.0.0.0/0',  # allow all users from anywhere
     }
 
     postgresql::server::db { 'postgres':
-      ip_mask_deny_postgres_user => '0.0.0.0/32', # don't deny from anywhere
-      ip_mask_allow_all_users    => '0.0.0.0/0',  # allow all users from anywhere
+      user     => 'dummy_user',                   # app will be connecting with the builtin Postgres user
+      password => 'dummy_password',
     }
   }
 
