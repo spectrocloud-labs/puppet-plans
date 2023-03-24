41108bd0a600bb2fc2de8e37e8bde0e5135946a7
Kevin Reeuwijk <kevin@spectrocloud.com>
2023-03-24 17:04:40 +0100

    Update script.sh
    Signed-off-by: Kevin Reeuwijk <kevin@spectrocloud.com>

--- spectro_hybrid/script.sh
+++ spectro_hybrid/script.sh
@@ -2,7 +2,7 @@
 wget https://apt.puppet.com/puppet-tools-release-focal.deb -O /tmp/puppet-tools-release-focal.deb
 dpkg -i /tmp/puppet-tools-release-focal.deb
 apt-get update
-apt-get install -y puppet-bolt qemu-guest-agent postgresql-client-common
+apt-get install -y puppet-bolt qemu-guest-agent
 mkdir /root/spectro_install
 cd /root/spectro_install
 bolt project init spectro_hybrid --modules puppetlabs-postgresql
